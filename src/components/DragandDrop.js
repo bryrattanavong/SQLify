33df9da6ddf00817bb7cd0bf1dfda0ddfd6b9965
Bryanrattanavong <brattanavong@interset.com>
2019-03-11 13:37:03 -0400

    initializing new component

---
+++ src/components/DragandDrop.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import styled from "styled-components";
+
+export default class DragandDrop extends Component{
+	state = {};
+	render(){
+		return(
+			<div>Hello World</div>
+		)
+	}
+}
